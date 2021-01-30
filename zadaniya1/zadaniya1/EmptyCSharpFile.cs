62c1e3e1af3d43d702fd3998f304d23b50057365
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-30 23:56:07 +0500

    Add zadaniya1/

---
+++ zadaniya1/zadaniya1/EmptyCSharpFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace zd
+{
+	static void Main(string[] args)
+	{
+		IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+
+		foreach (int num in squares)
+		{
+			Console.WriteLine(num);
+		}
+
+	}
+
+}
---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace zadaniya1
+{
+	class MainClass
+	{
+		public class Rectangle
+		{
+			double side1, side2;
+			public Rectangle(double side1, double side2)
+			{
+				this.side1 = side1;
+				this.side2 = side2;
+			}
+
+			public double AreaCalculator(double side1, double side2)
+			{
+				return side1 * side2;
+			}
+			public double PerimeterCalculator(double side1, double side2)
+			{
+				return (side1 + side2) * 2;
+			}
+			public double Area
+			{
+				get { return AreaCalculator(side1,side2); }
+			}
+			public double Perimetr
+			{
+				get { return PerimeterCalculator(side1,side2); }
+			}
+		}
+
+			static void Main(string[] args)
+			{
+			double sd1, sd2;
+			double.TryParse(Console.ReadLine(), out  sd1);
+			double.TryParse(Console.ReadLine(), out  sd2);
+			var rectangle = new Rectangle(sd1, sd2);
+			Console.WriteLine("Area: {0}, Perimeter: {1}", rectangle.Area, rectangle.Perimetr);
+			Console.ReadLine();
+			}
+	}
+	
+
+
+
+	}
